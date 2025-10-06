d1a9e15864ec369381bdb70c7190a08818bf4d6e
Aoi-git-Auto <ram772277psvita@gmail.com>
2025-10-06 11:40:27 +0900

    プレイヤー通常攻撃アルゴリズムの作成

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject attackObj;
+    [SerializeField] StatusData statusdata;
+    private float attack;
+    private float currentTime = 0f;
+    private Vector2 attackPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attack = statusdata.ATK;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attackPos = transform.parent.gameObject.transform.position;
+        currentTime += Time.deltaTime;
+        if (currentTime >= 2.0f)
+        {
+            currentTime = 0f;
+            Instantiate(attackObj, transform.position, Quaternion.identity);
+            attackObj.GetComponent<BatAttack>().SetAttack(attack);
+            attackObj.GetComponent<BatAttack>().SetPos(attackPos);
+        }
     }
 }
