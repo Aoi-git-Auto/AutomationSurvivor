005acb7c4842175112b6b6c772c85b8eb84d0adf
Aoi-git-Auto <ram772277psvita@gmail.com>
2025-10-12 23:14:19 +0900

    ボスHPバーの追加とボス用スクリプト作成

---
+++ Assets/Scripts/Enemy/BossEnemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : AbstractEnemy
+{
+    [SerializeField]
+    private GameObject hpBar;
+    private Vector3 diff;
+    private Vector3 direction;
+    
+    protected override void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        diff.x = playerPos.x - this.transform.position.x;
+        if (diff.x > 0)
+        {
+            direction = new Vector3(0, -180, 0);
+            this.transform.eulerAngles = direction;
+        }
+        if (diff.x < 0)
+        {
+            direction = new Vector3(0, 180, 0);
+            this.transform.eulerAngles = direction;
+        }
+    }
+}
