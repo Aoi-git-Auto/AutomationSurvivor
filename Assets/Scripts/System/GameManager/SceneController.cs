273edddd418189b6d00c1c78a5b445cd7f584d7d
Aoi-git-Auto <ram772277psvita@gmail.com>
2025-10-16 01:02:44 +0900

    SceneControllerを調整し、繰り返しプレイできる仕様に

--- Assets/Scripts/System/GameManager/SceneController.cs
+++ Assets/Scripts/System/GameManager/SceneController.cs
@@ -6,17 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
     [SerializeField]
-    private float time = 10f;
+    private float time = 300f;
     public void OnGame()
     {
         GMScript.instance.inGame = true;
         GMScript.instance.currentTime = time;
+        Time.timeScale = 1;
         SceneManager.LoadScene("GameScene");
     }
 
     public void OnRetry()
     {
         GMScript.instance.currentTime = time;
+        GMScript.instance.inGame = true;
+        Time.timeScale = 1;
         SceneManager.LoadScene("GameScene");
     }
 
