3adc437036a11fe6521774b4824e87a3f75a8b5a
Aoi-git-Auto <ram772277psvita@gmail.com>
2025-10-20 00:29:20 +0900

    プレイヤースピードアップスクリプトの追加

---
+++ Assets/Scripts/Items/SpeedItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class SpeedItem : AbstractStatusItem
+{
+    [SerializeField]
+    private float speedAmouunt;
+
+    protected override void Enhance(GameObject player)
+    {
+        player.GetComponent<PlayerController>().AddSpeed(speedAmouunt);
+        Destroy(this.gameObject);
+    }
+}
