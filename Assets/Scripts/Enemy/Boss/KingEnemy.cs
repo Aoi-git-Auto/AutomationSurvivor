8488472ce63724247355ee86ac0ee2e9931475a4
Aoi-git-Auto <ram772277psvita@gmail.com>
2025-10-29 23:58:45 +0900

    魔王の追加

---
+++ Assets/Scripts/Enemy/Boss/KingEnemy.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingEnemy : AbstractEnemy
+{
+    private enum AttackType
+    {
+        Warp,Beam,Hall
+    };
+    private List<AttackType> randomAttack = new List<AttackType>();
+    private float attackTime = 0f;
+
+    [SerializeField]
+    private GameObject warpGatePrehub;
+    [SerializeField]
+    private GameObject beamPrehub;
+    [SerializeField]
+    private GameObject itemEffectPrehub;
+    [SerializeField]
+    private GameObject hallPrehub;
+
+    [SerializeField]
+    private GameObject hpBar;
+    private BossHPBarContoroller bar;
+    private GameObject hp;
+    private GameObject bossGenerator;
+    private Animator animator;
+
+    [SerializeField]
+    private AudioClip dyingSE;
+    private AudioSource audioSource;
+
+    protected new void Start()
+    {
+        base.Start();
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        randomAttack.Add(AttackType.Warp);
+        randomAttack.Add(AttackType.Beam);
+        randomAttack.Add(AttackType.Hall);
+        GameObject canvas = GameObject.Find("UICanvas");
+
+        AudioManager.instance.PlayBossBGM(bgm);
+        bossGenerator = GameObject.Find("BossEnemyGenerator");
+        hp = Instantiate(hpBar, canvas.transform);
+        bar = hp.GetComponent<BossHPBarContoroller>();
+
+        bar.InitializeHPbar(MaxHP, this.name);
+    }
+
+    private void Update()
+    {
+        attackTime += Time.deltaTime;
+        if(attackTime > 5f)
+        {
+            AttackType type = randomAttack[Random.Range(0, randomAttack.Count)];
+            SelectAttack(type);
+            attackTime = 0f;
+        }
+    }
+
+    public override void Damage(float damage)
+    {
+        var effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.1f);
+        currentHP -= damage;
+        bar.UpdateHP(currentHP);
+        if (currentHP <= 0)
+        {
+            Destroy(hp);
+            if (bossGenerator != null)
+            {
+                bossGenerator.GetComponent<EnemyGenerator>().bossArrival = false;
+            }
+            audioSource.PlayOneShot(dyingSE);
+            AudioManager.instance.PlayinGameBGM();
+            Die();
+        }
+    }
+
+    private void SelectAttack(AttackType type)
+    {
+        switch (type)
+        {
+            case AttackType.Warp:
+                StartCoroutine(WarpToPlayer());
+                break;
+            case AttackType.Beam:
+                StartCoroutine(LaunchBeam());
+                break;
+            case AttackType.Hall:
+                StartCoroutine(SuctionHall());
+                break;
+        }
+    }
+
+    private IEnumerator WarpToPlayer()
+    {
+        canMove = false;
+        animator.enabled = false;
+        var effect = Instantiate(warpGatePrehub, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
+        spriteRenderer.enabled = false;
+        Destroy(effect);
+        transform.position = playerPos;
+        yield return new WaitForSeconds(0.5f);
+        effect = Instantiate(warpGatePrehub, transform.position, Quaternion.identity);
+        Destroy(effect, 0.5f);
+        spriteRenderer.enabled = true;
+        canMove = true;
+        animator.enabled = true;
+    }
+
+    private IEnumerator LaunchBeam()
+    {
+        canMove = false;
+        animator.enabled = false;
+        var effect = Instantiate(itemEffectPrehub, playerPos, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Destroy(effect);
+        Instantiate(beamPrehub, playerPos, Quaternion.identity);
+        canMove = true;
+        animator.enabled = true;
+    }
+    
+    private IEnumerator SuctionHall()
+    {
+        canMove = false;
+        animator.enabled = false;
+        var effect = Instantiate(itemEffectPrehub, playerPos, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Destroy(effect);
+        Instantiate(hallPrehub, playerPos, Quaternion.identity);
+        canMove = true;
+        animator.enabled = true;
+    }
+}
